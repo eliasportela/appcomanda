0a1c86ac7d7d27e3d2354440cace532d28380e0d
Elias Douglas <edwportela@gmail.com>
2018-07-09 13:45:50 -0300

    garcom-sel-produto

--- src/components/garcom/ComandaDetalhes.vue
+++ src/components/garcom/ComandaDetalhes.vue
@@ -34,12 +34,11 @@
 			</div>
 		</div>
 		<div class="w3-bottom container-btn-garcom">
-			<button class="w3-button w3-round w3-block w3-red btn-garcom">
+			<button class="w3-button w3-round w3-block w3-red btn-garcom" @click="toogleModalInsercao">
 				ADICIONAR
 			</button>
 		</div>
-
-		<div class="w3-modal">
+		<div class="w3-modal" :class="{'show':modalComanda}">
 			<div class="w3-modal-content w3-animate-opacity">
 				<top-bar></top-bar>
 				<div class="title-garcom">
@@ -77,75 +76,71 @@
 				</div>
 			</div>
 		</div>
-
-		<div class="w3-modal show">
+		<div class="w3-modal" :class="{'show':modalInsercao}">
 			<div class="w3-modal-content w3-animate-opacity">
 				<top-bar></top-bar>
 				<div class="title-garcom">
 					SELECIONE O TIPO DO PRODUTO
 				</div>
 				<div class="container-garcom garcom-tipo-produto">
-					<div class="w3-cell-row comanda-tipo" v-for="i in 3">
+					<div class="w3-cell-row comanda-tipo" v-for="i in 3" @click="selCategoria(i)">
 						<div class="w3-cell list-text">
 							Pizzas
 						</div>
 						<div class="w3-cell list-icon">
-							<i class="fa fa-check"></i>
+							<i class="fa fa-check" v-show="categoriaSelecionada == i"></i>
 						</div>
 					</div>
 				</div>
-				<div class="garcom-tipo-container w3-margin-top">	
-					<div class="w3-cell-row">
-						<div class="w3-cell cell-garcom-tipo">
-							<button class="w3-button w3-border btn-garcom-tipo">
-								<i class="far fa-circle"></i><br>
-								ESFIHA
-							</button>
-						</div>
-						<div class="w3-cell cell-garcom-tipo">
-							<button class="w3-button w3-border btn-garcom-tipo">
-								<i class="far fa-circle"></i><br>
-								BROTO
-							</button>
-						</div>
-						<div class="w3-cell cell-garcom-tipo">
-							<button class="w3-button w3-border w3-round btn-garcom-tipo">
-								<i class="far fa-circle"></i><br>
-								GRANDE
-							</button>
-						</div>
-					</div>
+				<div class="garcom-tipo-container w3-margin-top" v-show="categoriaSelecionada == 1">	
 					<hr class="garcom-hr">
 					<div class="w3-cell-row">
 						<div class="w3-cell cell-garcom-tipo">
-							<button class="w3-button w3-border btn-garcom-tipo">
-								<i class="far fa-circle"></i><br>
-								1/3
-							</button>
-						</div>
-						<div class="w3-cell cell-garcom-tipo">
-							<button class="w3-button w3-border btn-garcom-tipo">
-								<i class="far fa-circle"></i><br>
+							<button class="w3-button w3-border btn-garcom-tipo" @click="selTipoPizza(1)">
+								<i :class="[tipoPizza == 1 ? 'fas fa-check-circle' : 'far fa-circle']"></i><br>
 								1/2
 							</button>
 						</div>
 						<div class="w3-cell cell-garcom-tipo">
-							<button class="w3-button w3-border w3-round btn-garcom-tipo">
-								<i class="far fa-circle"></i><br>
+							<button class="w3-button w3-border w3-round btn-garcom-tipo" @click="selTipoPizza(2)">
+								<i :class="[tipoPizza == 2 ? 'fas fa-check-circle' : 'far fa-circle']"></i><br>
 								INTEIRA
 							</button>
 						</div>
 					</div>
 				</div>
 				<div class="w3-bottom container-btn-garcom">
-					<button class="w3-button w3-round w3-red w3-block btn-garcom">
+					<button class="w3-button w3-round w3-red w3-block btn-garcom" :disabled="!avancar" @click="avancarModal(2)">
 						PRÓXIMO
 						<i class="fa fa-chevron-right"></i>
 					</button>
 				</div>
 			</div>
 		</div>
-
+		<div class="w3-modal" :class="{'show':modalProduto}">
+			<div class="w3-modal-content">
+				<top-bar></top-bar>
+				<div class="title-garcom">
+					SELECIONE O PRODUTO
+				</div>
+				<div class="container-garcom garcom-tipo-produto">
+					<div class="w3-cell-row comanda-tipo" v-for="i in 10" @click="selProduto(i)">
+						<div class="w3-cell list-text">
+							{{i}} - Mozzarela
+						</div>
+						<div class="w3-cell list-icon">
+							<i class="fa fa-check" v-show="produtoSelecionado == i"></i>
+						</div>
+					</div>
+				</div>
+				<div class="w3-bottom container-btn-garcom">
+					<button class="w3-button w3-round w3-red w3-block btn-garcom" :disabled="!avancar">
+						PRÓXIMO
+						<i class="fa fa-chevron-right"></i>
+					</button>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -160,8 +155,15 @@ import TopBar from "../commons/TopBar.vue"
 		components:{TopBar/*,ModalProduto*/},
 		data(){
 		    return{
-		      comanda:"",
-		      produtos:[]
+		    	modalComanda:false,
+		    	modalInsercao:false,
+		    	categoriaSelecionada:0,
+		    	produtoSelecionado:0,
+		    	tipoPizza:0,
+		      	comanda:"",
+		      	produtos:[],
+		      	avancar:false,
+		      	modalProduto:false
 		    }
 	  },
 	  methods:{
@@ -170,6 +172,37 @@ import TopBar from "../commons/TopBar.vue"
 		      .then(response => {
 		        this.produtos = response.data;
 		      });
+	  	},
+	  	toogleModalInsercao(){
+	  		this.modalInsercao = !this.modalInsercao
+	  	},
+	  	selCategoria(id){
+	  		this.categoriaSelecionada = id;
+	  		if (this.categoriaSelecionada != 1) {
+	  			this.avancar = true;
+	  		}else{
+	  			this.avancar = false;
+	  			if (this.tipoPizza > 0) {
+	  				this.avancar = true;	
+	  			}
+	  		}
+	  	},
+	  	selTipoPizza(id){
+	  		this.tipoPizza = id;
+	  		this.avancar = true;
+	  	},
+	  	selProduto(id){
+	  		this.produtoSelecionado = id;
+	  		this.avancar = true;
+	  	},
+	  	avancarModal(modal){
+	  		if (modal == 1) {
+	  			this.modalInsercao = true;
+	  			this.modalProduto = false;
+	  		} else if (modal == 2){
+	  			this.modalInsercao = false;
+	  			this.modalProduto = true;
+	  		}
 	  	}
 	  },
 	  created: function () {
@@ -186,6 +219,10 @@ import TopBar from "../commons/TopBar.vue"
 .list-icon{
 	width: 20%;
 	text-align: center;
+}
+.w3-button:hover{
+	background-color: #fff!important;
+    color: #000!important;
 }
 
 </style>
